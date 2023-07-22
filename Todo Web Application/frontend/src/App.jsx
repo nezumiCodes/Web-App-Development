225b6d3c3bdac01c1f08e013582088f872de076e
Nezumi <nezumi.shix@gmail.com>
2023-07-22 13:50:23 +0100

    Creating React pages, Components and styling

--- Todo Web Application/frontend/src/App.jsx
+++ Todo Web Application/frontend/src/App.jsx
@@ -1,26 +1,50 @@
-import './styles/App.css';
-import Welcome from './pages/Welcome';
+import './styles/style.scss';
 import Home from './pages/Home';
+import AddTask from './pages/AddTask';
+import EditTask from './pages/EditTask';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+// import Welcome from './components/Welcome';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet /> {/* Outlet is like an empty space used to render the content of pages, acts like a placeholder*/}
+      <Footer />
+    </div>
+  )
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }, 
+      {
+        path: "/add_task", 
+        element: <AddTask />
+      }, 
+      {
+        path: "/edit_task/:id", 
+        element: <EditTask />
+      }
+    ]
+  }
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Welcome name='Celia'/>
-        <Home />
-        <h1>Hello World!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+         <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
--- Todo Web Application/frontend/src/pages/Home.jsx
+++ Todo Web Application/frontend/src/pages/Home.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 export default function Home() {
     return(
         <div className="home">
-            <nav className="navbar navbar-dark navbar-fixed-top">
-                <Link to="/" className="navbar-brand">Todo App</Link>
-            </nav>
             <div className="container">
                 <h1>Home</h1>
             </div>
